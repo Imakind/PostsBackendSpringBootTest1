2a545ac6ad38dc3e3a45ba4fcdec388d10fb874f
Akishev Akzhan <akishevakzhan@gmail.com>
2025-03-27 16:55:02 +0500

    Full Lesson web app

--- src/main/java/com/akzhan/springmvcApp/controllers/BlogController.java
+++ src/main/java/com/akzhan/springmvcApp/controllers/BlogController.java
@@ -46,4 +46,30 @@ public class BlogController {
         model.addAttribute("post", result);
         return "blog-detail";
     }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogEdit(Model model, @PathVariable(value="id") long id) {
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> result = new ArrayList<>();
+        post.ifPresent(result::add);
+        model.addAttribute("post", result);
+        return "blog-edit";
+    }
+
+    @PostMapping("/blog/{id}/edit")
+    public String blogUpdatePost(@PathVariable(value="id") long id, @RequestParam String title, @RequestParam String anons, @RequestParam String full_text) {
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setAnons(anons);
+        post.setFull_text(full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+    @PostMapping("/blog/{id}/delete")
+    public String blogDeletePost(@PathVariable(value="id") long id) {
+        Post post = postRepository.findById(id).orElseThrow();
+        postRepository.delete(post);
+        return "redirect:/blog";
+    }
 }
